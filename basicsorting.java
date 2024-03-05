ff77ff1b11e98426bf9787ff7c2bbc07c369efc2
nandani74 <nandaniraut7@gmail.com>
2024-03-05 17:56:37 +0530

    commited succesfully

---
+++ basicsorting.java
@@ -0,0 +1,54 @@
+import java.util.*;
+public class basicsorting {
+    //bubblesort
+    public static void bubblesort(int arr[]){
+        for(int turn=0; turn<arr.length-1; turn++){
+            for(int j=0; j<arr.length-1-turn; j++){
+                if(arr[j+1] <arr[j]){
+                    //swap
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1] =temp;
+                }
+            }
+        }
+    }
+    public static void selectionsort(int arr[]){
+        for(int i=0; i<arr.length-1; i++){
+            int minpos=i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[minpos] > arr[j]){
+                    minpos=j;
+                }
+            }
+            //swap
+            int temp=arr[minpos];
+            arr[minpos]=arr[i];
+            arr[i]=temp;
+        }
+    }
+    public static void insertionsort(int arr[]){
+        for(int i=0; i<arr.length-1; i++){
+            int curr=arr[i];
+            int prev = i-1;
+            while(prev >=0 && arr[prev] > curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            arr[prev+1] = curr;
+
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[]={4,3,2,5,1,7,8,6,9};
+        insertionsort(arr);
+        printArr(arr);
+    }
+}
