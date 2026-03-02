64299acc8d4540ffb6134558bf2ee4e4249041d9
Nandani Raut <160380990+nandani74@users.noreply.github.com>
2026-03-02 12:31:49 +0530

    Add files via upload
    reverse an array

---
+++ Arrays/Reverse.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class Reverse {
+    public static void ReverseArr(int arr[]){
+        int first = 0; 
+        int last = arr.length-1;
+        while(first < last){
+            int temp = arr[last];
+            arr[last] = arr[first];
+            arr[first] = temp;
+            first++;
+            last--;
+        }
+
+    }
+    public static void main(String args[]){
+        int arr[] = {2,4,6,8,10};
+        ReverseArr(arr);   
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
